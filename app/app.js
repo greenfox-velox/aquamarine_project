f55a4aa55569e5947f335a971aaae62f4e64d8e2
Szemann Peter <szemannp@gmail.com>
2016-08-02 10:47:48 +0200

    linterfixed

--- app/app.js
+++ app/app.js
@@ -4,14 +4,14 @@ CalorieCounter.directive('mealTable', function() {
   return {
     restrict: 'E',
     templateUrl: '../mealtable.html'
-  }
+  };
 });
 
 CalorieCounter.factory('Config', function() {
   return {
     baseUrl: 'http://localhost:3000/meals',
-    extendedUrl: 'http://localhost:3000/meals/',
-  }
+    extendedUrl: 'http://localhost:3000/meals/'
+  };
 });
 
 CalorieCounter.factory('CalorieService', function(Config, $http) {
@@ -22,48 +22,47 @@ CalorieCounter.factory('CalorieService', function(Config, $http) {
     deleteMeal: function(item) {
       return $http.delete(Config.extendedUrl + item);
     },
-    addMeal: function(newMeal){
+    addMeal: function(newMeal) {
       return $http.post(Config.extendedUrl, newMeal);
     }
-  }
+  };
 });
 
-CalorieCounter.controller('AppController', function($scope, $http, CalorieService){
+CalorieCounter.controller('AppController', function($scope, $http, CalorieService) {
   $scope.meals = [];
 
   $scope.addMeal = function() {
     var newMeal = {
-        name: $scope.newMeal.name,
-        calories: $scope.newMeal.calories,
-        date: $scope.newMeal.date
-      };
-    CalorieService.addMeal(newMeal).success(function(data){
+      name: $scope.newMeal.name,
+      calories: $scope.newMeal.calories,
+      date: $scope.newMeal.date
+    };
+    CalorieService.addMeal(newMeal).success(function(data) {
       $scope.meals.push(data);
-     })
-  }
+    });
+  };
 
-  $scope.deleteMeal = function (item) {
+  $scope.deleteMeal = function(item) {
     var index = $scope.meals.indexOf(item);
-    CalorieService.deleteMeal(item.id).success(function(){
-       $scope.meals.splice(index, 1);
-     })
-  }
+    CalorieService.deleteMeal(item.id).success(function() {
+      $scope.meals.splice(index, 1);
+    });
+  };
 
-  $scope.getSum = function(){
-      var sum = 0;
-      $scope.meals.forEach(function (meal) {
-        sum += meal.calories;
-      })
-      return sum;
-      }
+  $scope.getSum = function() {
+    var sum = 0;
+    $scope.meals.forEach(function(meal) {
+      sum += meal.calories;
+    });
+    return sum;
+  };
 
-  $scope.reload = function(){
-    $scope.search = "";
-   };
+  $scope.reload = function() {
+    $scope.search = '';
+  };
 
   CalorieService.getAll().success(function(data) {
     $scope.meals = data;
     $scope.loading = false;
-  }).finally;
-
+  });
 });
--- backend/db.js
+++ backend/db.js
@@ -1,46 +1,43 @@
 'use strict';
 
-var data = require('./config.js');
+var config = require('./config.js');
 
-function handleError (err) {
-  if(err) {
+function handleError(err) {
+  if (err) {
     console.log(err.toString());
     return;
   }
 }
 
 var serverQueries = {
-    getMeals: 'SELECT * FROM meals',
-    postMeals: 'INSERT INTO meals SET ?',
-    deleteMeals: 'DELETE FROM meals WHERE id = ?'
-  };
+  getMeals: 'SELECT * FROM meals',
+  postMeals: 'INSERT INTO meals SET ?',
+  deleteMeals: 'DELETE FROM meals WHERE id = ?'
+};
 
-var readTheFunctions = (function () {
-
+var readTheFunctions = (function() {
   var mysql = require('mysql');
 
-  var con = mysql.createConnection(data);
+  var con = mysql.createConnection(config);
 
   con.connect();
 
-  function readAll (data, callback) {
-    con.query(serverQueries.getMeals, function(err,rows){
+  function readAll(data, callback) {
+    con.query(serverQueries.getMeals, function(err, rows) {
       handleError(err);
       callback(err, rows);
     });
   }
 
   function createNewMeal(item, callback) {
-    console.log(item);
-    con.query(serverQueries.postMeals, item, function(err, rows){
+    con.query(serverQueries.postMeals, item, function(err, rows) {
       handleError(err);
-      console.log(rows);
-      callback(err, {id: rows.insertId, name: item.name, calories: item.calories, date: item.date})
+      callback(err, {id: rows.insertId, name: item.name, calories: item.calories, date: item.date});
     });
   }
 
   function deleteCalorie(id, callback) {
-    con.query(serverQueries.deleteMeals, id, function(err,rows){
+    con.query(serverQueries.deleteMeals, id, function(err) {
       handleError();
       callback(err, {id: id});
     });
@@ -50,7 +47,7 @@ var readTheFunctions = (function () {
     readAll: readAll,
     createNewMeal: createNewMeal,
     deleteCalorie: deleteCalorie
-  }
+  };
 })();
 
 module.exports = readTheFunctions;
