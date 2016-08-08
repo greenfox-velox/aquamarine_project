20a8dac22679e6ddf979064f03a3b286f25f5aa9
Biro Sandor <biro.sandor.8@gmail.com>
2016-08-08 14:11:55 +0200

    gulpfile refactor

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,11 @@ var sass = require('gulp-sass');
 var mocha = require('gulp-mocha');
 var KarmaServer = require('karma').Server;
 var browserSync = require('browser-sync').create();
-  // uglify = require('gulp-uglify'),
-// var exec = require('child_process').exec;
-// sourceMaps = require('gulp-sourcemaps'),
 
-// gulp.task('test', function (){
-//   gulp.src('spec/*.js')
-//     .pipe(uglify())
-//     .pipe(gulp.dest('minjs'))
-// });
-//
 gulp.task('supertest', function() {
   gulp.src('spec/backendtest.js')
   .pipe(mocha());
 });
-
-// gulp.task('karma', function (cb) {
-//   exec('karma start', function (err, stdout, stderr) {
-//     console.log(stdout);
-//     console.log(stderr);
-//     cb(err);
-//   });
-// });
 
 gulp.task('karma', function(done) {
   new KarmaServer({
@@ -32,7 +15,6 @@ gulp.task('karma', function(done) {
     singleRun: true
   }, done).start();
 });
-
 
 gulp.task('style', function() {
   gulp.src('client/**/*scss', {base: 'client/assets/css'})
@@ -42,24 +24,14 @@ gulp.task('style', function() {
 
 gulp.task('sass-watch', ['style'], browserSync.reload);
 
-gulp.task('watch', function() {
+gulp.task('browserSync', function() {
   browserSync.init({
-    server: {
-      baseDir: 'client/'
-    }
+    proxy: 'http://localhost:3000',
+    port: 7000
   });
-
   gulp.watch('backend/*.js', ['supertest']);
   gulp.watch('client/*.js', ['karma']);
   gulp.watch('client/**/*scss', ['sass-watch']);
 });
 
-// gulp.task('browserSync', function() {
-//  browserSync.init({
-//    proxy: 'http://localhost:3000',
-//    port: 7000
-//  });
-// });
-
-// gulp.task('default', ['supertest', 'karma', 'style', 'watch']);
-gulp.task('default', ['watch']);
+gulp.task('default', ['browserSync']);
