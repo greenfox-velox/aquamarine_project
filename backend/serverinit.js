8054612a5a7f091ce78bf52d22a303b949f7a708
Szemann Peter <szemannp@gmail.com>
2016-08-08 13:34:39 +0200

    header fix

--- backend/serverinit.js
+++ backend/serverinit.js
@@ -3,9 +3,5 @@ var mysqlconnection = require('./mysqlconnection.js')
 
 
 var app = api.createApp(mysqlconnection)
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+
 app.listen(3000);
